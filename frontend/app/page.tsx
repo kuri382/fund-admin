0ad86c3ef772f467bae4b637580055e8e143a37e
Tetsu.Kuribayashi <tktkchestnut[gmail.com>
2024-08-15 15:45:02 +0900

    :wrench: (frontned) setup nextjs

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to My Web App</h1>
+      <p>This is the homepage of our amazing web application!</p>
+      <button style={{ padding: '10px 20px', fontSize: '16px' }}>Get Started</button>
+    </div>
+  );
+}
+
+export default HomePage;
