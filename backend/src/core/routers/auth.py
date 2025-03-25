fef25a7c1469b9802fa86eab53abe9f2f7550279
Tetsu.Kuribayashi <tktkchestnut[gmail.com>
2025-03-26 03:44:14 +0900

    :sparkles: (backend) integrated invitation

--- backend/src/core/routers/auth.py
+++ backend/src/core/routers/auth.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter  # , Depends, HTTPException
+import logging
+
+from fastapi import APIRouter, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel, EmailStr
 
-# from fastapi import APIRouter  # , Depends, HTTPException
-# from firebase_admin import auth as firebase_auth
-# from jose import JWTError, jwt
-# from src.core.services.auth_service import create_access_token
-# from src.settings import settings
+from ._base import BaseJSONSchema
 
-router = APIRouter()
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class SignUpRequest(BaseModel):
@@ -29,62 +33,34 @@ class TokenResponse(BaseModel):
     token_type: str = "bearer"
 
 
-'''
-@router.post("/signup", response_model=TokenResponse)
-async def signup(signup_request: SignUpRequest, firebase=Depends(get_firebase)):
-    try:
-        # Firebaseで新規ユーザー作成
-        user = firebase_auth.create_user(
-            email=signup_request.email,
-            password=signup_request.password
-        )
+INVITATION_CODES = {
+    "S39H23": True,
+    "XZ98H7": True,
+    "RX39DS": True,
+    "DE332X": True,
+    "A35VS0": True,
+}
 
-        # JWTトークンの発行
-        access_token = create_access_token({"uid": user.uid})
-
-        firebase.firestore.collection('users').document(user.uid).set({
-            "email": signup_request.email,
-            "uid": user.uid,
-            "created_at": "test"
-        })
-
-        return {"access_token": access_token}
-    except firebase_auth.EmailAlreadyExistsError:
-        raise HTTPException(status_code=400, detail="Email already exists")
-    except Exception as e:
-        raise HTTPException(status_code=400, detail="Sign-up failed")
+class ReqInvitationCheck(BaseJSONSchema):
+    invitationCode: str
 
 
-@router.post("/signin", response_model=TokenResponse)
-async def login(login_request: LoginRequest, firebase=Depends(get_firebase)):
-    """
-    クライアントから送信されたFirebase IDトークンを検証し、
-    ユーザーが認証されている場合にJWTアクセストークンを発行する。
-    """
-    try:
-        # 1. Firebase IDトークンを検証
-        decoded_token = firebase_auth.verify_id_token(login_request.id_token)
+class ResInvitationCheck(BaseJSONSchema):
+    message: str
 
-        # 2. ユーザーのUIDを取得
-        uid = decoded_token.get('uid')
 
-        if not uid:
-            raise HTTPException(status_code=400, detail="Invalid token")
-
-        # 3. JWTアクセストークンを生成
-        access_token = create_access_token({"uid": uid})
-
-        return {"access_token": access_token}
-
-    except firebase_auth.InvalidIdTokenError:
-        raise HTTPException(status_code=401, detail="Invalid ID token")
-
-    except firebase_auth.ExpiredIdTokenError:
-        raise HTTPException(status_code=401, detail="Expired ID token")
-
-    except firebase_auth.RevokedIdTokenError:
-        raise HTTPException(status_code=401, detail="Revoked ID token")
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Login failed: An unexpected error occurred")
-'''
+@router.post(
+    "/invitation/check",
+    response_model=ResInvitationCheck,
+    status_code=status.HTTP_200_OK
+)
+async def post_auth_check_invitation(
+    req: ReqInvitationCheck,
+):
+    invitation_code = req.invitationCode
+    if invitation_code not in INVITATION_CODES or not INVITATION_CODES[invitation_code]:
+        raise HTTPException(status_code=400, detail="招待コードが無効です")
+    content = ResInvitationCheck(
+        message="招待コードが確認されました"
+    )
+    return ORJSONResponse(content=jsonable_encoder(content))
